fdcfb958c63f52f9955dd65cc7328752c3c1314c
blackteahamburger <blackteahamburger@outlook.com>
2020-04-14 19:48:55 +0800

    Add install.sh and config.sh, update README.md

---
+++ config.sh
@@ -0,0 +1,13 @@
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+hwclock --systohc
+nano /etc/locale.gen
+locale-gen
+nano /etc/locale.conf
+nano /etc/hostname
+nano /etc/hosts
+passwd
+pacman -S grub efibootmgr os-prober intel-ucode dosfstools ntfs-3g
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+wifi-menu
+timedatectl set-ntp true
+fdisk -l
+mkfs.ext4 /dev/sdXx
+mkfs.fat -F 32 /dev/sdXx
+mount /dev/sdXx /mnt
+mkdir /mnt/boot
+mount /dev/sdXx /mnt/boot
+nano /etc/pacman.d/mirrorlist
+pacstrap /mnt base linux-zen linux-firmware nano
+genfstab -U /mnt >> /mnt/etc/fstab
