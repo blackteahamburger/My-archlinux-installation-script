09668264c06a39c0d3ed696d0fb94fcdee4d1f33
blacktea <blackteahamburger@outlook.com>
2020-04-17 22:09:10 +0800

    Update config.sh and install.sh

--- config.sh
+++ config.sh
@@ -1,32 +1,15 @@
-#!/bin/bash
+ln -sf /usr/share/zoneinfo/yourregion/yourcity /etc/localtime
+hwclock --systohc
+sed -i '/yourlocale/{s/#//}' /etc/locale.gen
+locale-gen
+echo "yourlocale" >> /etc/locale.conf
+echo "yourhostname" >> /etc/hostname
+echo "127.0.0.1 localhost
+::1             localhost
+127.0.1.1       yourhostname.yourlocaldomain  yourhostname" >> /etc/hosts
+echo root:yourpasswd | chpasswd
+pacman -S grub efibootmgr os-prober yourCPU-ucode dosfstools ntfs-3g
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
 
-while getopts "h:c" opt;do
- case ${opt} in
-  h) help ;;
-  c) configure ;;
-  *) help ;;
- esac
-done
-
-help(){
- echo "Options:"
- echo "-c Configure Arch Linux"
- echo "-h Display this help"
-}
-
-configure(){
- ln -sf /usr/share/zoneinfo/yourregion/yourcity /etc/localtime
- hwclock --systohc
- sed -i '/yourlocale/{s/#//}' /etc/locale.gen
- locale-gen
- echo "yourlocale" >> /etc/locale.conf
- echo "yourhostname" >> /etc/hostname
- echo "127.0.0.1 localhost
- ::1             localhost
- 127.0.1.1       yourmyhostname.yourlocaldomain  yourmyhostname" >> /etc/hosts
- echo root:yourpasswd | chpasswd
- pacman -S grub efibootmgr os-prober yourCPU(Intel or AMD)-ucode dosfstools ntfs-3g
- grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
- grub-mkconfig -o /boot/grub/grub.cfg
- exit 
-}
--- install.sh
+++ install.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-while getopts "h:i" opt;do
- case ${opt} in
-  h) help ;;
-  i) install ;;
-  *) help ;;
- esac
-done
+wifi-menu
+timedatectl set-ntp true
+fdisk -l
+mkfs.ext4 /dev/sdXx(yourpartition)
+mkfs.fat -F 32 /dev/sdXx(yourpartition)
+mount /dev/sdXx(yourpartition) /mnt
+mkdir /mnt/boot
+mount /dev/sdXx(yourpartition) /mnt/boot
+sed -i '/yourlocale/!{n;/Server/s/^/#/};t;n' /etc/pacman.d/mirrorlist
+pacstrap /mnt base linux-zen linux-firmware nano
+genfstab -U /mnt >> /mnt/etc/fstab
 
-help(){
- echo "Options:"
- echo "-i Installing Arch Linux"
- echo "-h Display this help"
-}
-
-install(){
- wifi-menu
- timedatectl set-ntp true
- fdisk -l
- mkfs.ext4 /dev/sdXx(yourpartition)
- mkfs.fat -F 32 /dev/sdXx(yourpartition)
- mount /dev/sdXx(yourpartition) /mnt
- mkdir /mnt/boot
- mount /dev/sdXx(yourpartition) /mnt/boot
- sed -i '/yourlocale/!{n;/Server/s/^/#/};t;n' /etc/pacman.d/mirrorlist
- pacstrap /mnt base linux-zen linux-firmware nano
- genfstab -U /mnt >> /mnt/etc/fstab
-}
