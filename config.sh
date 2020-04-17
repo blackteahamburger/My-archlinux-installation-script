537260e14c9ec6ca03636fa8dd0083489b1a0334
blacktea <blackteahamburger@outlook.com>
2020-04-17 20:50:30 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ echo "127.0.0.1	localhost
 ::1		localhost
 127.0.1.1	yourmyhostname.yourlocaldomain	yourmyhostname" >> /etc/hosts
 echo root:yourpasswd | chpasswd
-pacman -S grub efibootmgr os-prober intel-ucode dosfstools ntfs-3g
+pacman -S grub efibootmgr os-prober yourCPU(Intel or AMD)-ucode dosfstools ntfs-3g
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 exit
