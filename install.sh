cb5d651154b828d4559edc7ca42f48d76ebf5f7a
blacktea <blackteahamburger@outlook.com>
2020-04-17 22:13:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 wifi-menu
 timedatectl set-ntp true
 fdisk -l
-mkfs.ext4 /dev/sdXx(yourpartition)
-mkfs.fat -F 32 /dev/sdXx(yourpartition)
-mount /dev/sdXx(yourpartition) /mnt
+mkfs.ext4 /dev/yourpartition
+mkfs.fat -F 32 /dev/yourpartition
+mount /dev/yourpartition /mnt
 mkdir /mnt/boot
-mount /dev/sdXx(yourpartition) /mnt/boot
+mount /dev/yourpartition /mnt/boot
 sed -i '/yourlocale/!{n;/Server/s/^/#/};t;n' /etc/pacman.d/mirrorlist
 pacstrap /mnt base linux-zen linux-firmware nano
 genfstab -U /mnt >> /mnt/etc/fstab
